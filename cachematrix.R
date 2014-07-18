fcb23a7e97ebe4cd2d717b39692a7f1be8a81a3c
JayLKim <Jay.Luke.Kim@gmail.com>
2014-07-18 13:14:25 +0900

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 'makeCacheMatrix' function creates a list that consists of functions to 
+## set the value of a matrix and the caculated value of an inverse matrix
+## into a cache; and to get the cached value of a matrix and the caculated 
+## result out. The 'cacheSolve' function is to compute the inverse of the matrix
+## cached by the previous function.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## To create a list of functions: set(), get(), setSolve(), getSolve()
+makeCacheMatrix <- function(x = matrix()) 
+{
+        I <- NULL ## variable for caching an Inverse
+        set <- function(y)
+        {
+                x <<- y ## To assign the input value to 'x' defined 
+                        ## at the parent environment
+                I <<- NULL
+        }
+        get <- function()
+        {
+                x
+        }
+        setSolve <- function(Solve)
+        {
+                I <<- Solve ## To cache an inverse
+        }
+        getSolve <- function()
+        {
+                I
+        }
+        list(set=set, get=get, setSolve=setSolve, getSolve=getSolve)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## To compute the inverse of a matrix. If it has already been computed and cached,
+## the function will return that value.
+cacheSolve <- function(x, ...)
+{
+        I <- x$getSolve()
+        if(!is.null(I)) ## To check whether there is the caculated value or not
+        {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get() 
+        I <- solve(data, ...) ## if not, the 'solve' function will compute the inverse
+        x$setSolve(I) ## To cache the inverse
+        I ## To return a matrix that is the inverse of 'x'
 }
